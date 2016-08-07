16ff55cdefd3c61cb6ae8b0f2f029fc4894303e2
coral <izjpbd@gmail.com>
2016-08-07 13:44:31 +0000

    add initial setups

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module MicropostsGodzilla
+module Microposts
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -22,5 +22,12 @@ module MicropostsGodzilla
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+      config.action_view.embed_authenticity_token_in_remote_forms = true
+    end
   end
 end
